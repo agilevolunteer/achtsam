448be73e2c2dc2c3065f63469d6c5b00c89e33ba
Marcel Henning <marcel.henning@neuland-bfi.de>
2015-09-23 12:40:33 +0200

    Added vagrantpress for development purposes

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,1 +1,8 @@
 svn co https://themes.svn.wordpress.org/i-excel/1.1.4/ iexcel
+git clone https://github.com/chad-thompson/vagrantpress.git development
+cd development
+vagrant up
+cd ..
+cp -avr iexcel/ development/wordpress/wp-content/themes/iexcel
+
+
